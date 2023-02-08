8515c1f034bb97b78168d2d76703dd377f5a25f0
Anton <antonbaranskij12@gmail.com>
2023-02-08 13:18:36 +0200

    Init project

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Wrapper } from "./HeaderStyled";
+
+const Header = ()=>{
+    return(
+        <Wrapper>
+            <p>dsaaaaaaaaaaaadsfjkdjaskj</p>
+        </Wrapper>
+    )
+}
+
+export default Header;
---
+++ src/components/Header/HeaderStyled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  border: 1px solid black;
+  height: 800px;
+`;
